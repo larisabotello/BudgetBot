5a2a9c9a4364ff8c897a3c6338d028f985257dfe
Matthew Martinez <mattmartinez3n@gmail.com>
2019-02-16 21:00:09 -0600

    Outgoing Feature
    Made changes to outgoing ts and html

--- Getbud/src/app/outgoing/outgoing.component.spec.ts
+++ Getbud/src/app/outgoing/outgoing.component.spec.ts
@@ -1,22 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { OutgoingPage } from './outgoing.page';
+import { OutgoingComponent } from './outgoing.component';
 
-describe('OutgoingPage', () => {
-  let component: OutgoingPage;
-  let fixture: ComponentFixture<OutgoingPage>;
+describe('OutgoingComponent', () => {
+  let component: OutgoingComponent;
+  let fixture: ComponentFixture<OutgoingComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OutgoingPage ],
+      declarations: [ OutgoingComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(OutgoingPage);
+    fixture = TestBed.createComponent(OutgoingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
