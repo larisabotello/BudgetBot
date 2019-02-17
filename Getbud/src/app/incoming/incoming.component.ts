8e4fbea079efbee95ef4445e3912ce7a77e6a404
Larisa Botello <>
2019-02-17 10:11:37 -0600

    ionic storage of incoming and outgoing data

--- Getbud/src/app/incoming/incoming.component.ts
+++ Getbud/src/app/incoming/incoming.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddIncomingComponent } from '../add-incoming/add-incoming.component';
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'app-incoming',
   templateUrl: './incoming.component.html',
   styleUrls: ['./incoming.component.scss'],
 })
 export class IncomingComponent implements OnInit {
-
-  constructor(public dialog: MatDialog) { }
+  inKey = 'incomingData';
+  constructor(public dialog: MatDialog, private storage: Storage) { }
   allIncomingData: {title: string, date: Date, amount: number}[] = [];
-  ngOnInit() {}
+  ngOnInit() {
+    this.getStorageData();
+  }
   addIncoming() {
     const incomingDialog = this.dialog.open(AddIncomingComponent, {
       disableClose: true,
@@ -18,11 +21,25 @@ export class IncomingComponent implements OnInit {
     });
     incomingDialog.afterClosed().subscribe(data => {
       if(data !== null){
-        this.allIncomingData.push(data)
+        if(this.allIncomingData === null) {
+          this.allIncomingData = [data];
+        } else {
+          this.allIncomingData.push(data)
+        }
+        this.storage.set(this.inKey, this.allIncomingData);
       }
     });
   }
   delete(index) {
     this.allIncomingData.splice(index, 1);
+  }
+  getStorageData() {
+    try{
+      this.storage.get(this.inKey).then((data) =>{
+        this.allIncomingData = data
+      });
+    } catch {
+
+    }
   }
 }
--- Getbud/src/app/outgoing/outgoing.component.ts
+++ Getbud/src/app/outgoing/outgoing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddOutgoingComponent } from '../add-outgoing/add-outgoing.component';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-outgoing',
@@ -8,8 +9,8 @@ import { AddOutgoingComponent } from '../add-outgoing/add-outgoing.component';
   styleUrls: ['./outgoing.component.scss'],
 })
 export class OutgoingComponent implements OnInit {
-
-  constructor(public dialog: MatDialog) { }
+  outKey = 'outgoingData';
+  constructor(public dialog: MatDialog, private storage: Storage) { }
   allOutgoingData: {title: string, date: Date, amount: number}[] = [];
   ngOnInit() {}
 
@@ -20,12 +21,26 @@ export class OutgoingComponent implements OnInit {
     });
     outgoingDialog.afterClosed().subscribe(data =>{
       if(data !== null){
-        this.allOutgoingData.push(data)
+        if(this.allOutgoingData === null) {
+          this.allOutgoingData = [data];
+        } else {
+          this.allOutgoingData.push(data)
+        }
+        this.storage.set(this.outKey, this.allOutgoingData);
       }
     });
   }
   delete(index) {
     this.allOutgoingData.splice(index, 1);
+  }
+  getStorageData() {
+    try{
+      this.storage.get(this.outKey).then((data) =>{
+        this.allOutgoingData = data
+      });
+    } catch {
+
+    }
   }
 
 }
