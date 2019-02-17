e4069a5c3ec7a870afc70efe87bd9d51e5ec645a
Matthew Martinez <mattmartinez3n@gmail.com>
2019-02-17 10:49:27 -0600

    changed page to component

--- Getbud/src/app/budget/budget.component.spec.ts
+++ Getbud/src/app/budget/budget.component.spec.ts
@@ -1,22 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BudgetPage } from './budget.page';
+import { BudgetComponent } from './budget.component';
 
 describe('BudgetPage', () => {
-  let component: BudgetPage;
-  let fixture: ComponentFixture<BudgetPage>;
+  let component: BudgetComponent;
+  let fixture: ComponentFixture<BudgetComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BudgetPage ],
+      declarations: [ BudgetComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BudgetPage);
+    fixture = TestBed.createComponent(BudgetComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
